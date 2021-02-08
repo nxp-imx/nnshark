8a65a00af21f0dc5c0b26d910aeffc0cc305182a
Xavier Roumegue <xavier.roumegue@nxp.com>
2021-02-08 13:18:36 +0000

    tracers: gstctf: Add gpu usage print support
    Signed-off-by: Xavier Roumegue <xavier.roumegue@nxp.com>

--- plugins/tracers/gstctf.c
+++ plugins/tracers/gstctf.c
@@ -763,6 +763,49 @@ do_print_cpuusage_event (event_id id, guint32 cpu_num, gfloat * cpuload)
   g_mutex_unlock (&ctf_descriptor->mutex);
 }
 
+do_print_gpuusage_event (event_id id, guint32 cpu_num, gfloat * gpuload)
+{
+  GError *error;
+  guint8 *mem;
+  guint8 *event_mem;
+  gsize event_size;
+  gint gpu_idx;
+
+  event_size = gpu_num * sizeof (gfloat) + CTF_HEADER_SIZE;
+
+  if (event_exceeds_mem_size (event_size)) {
+    return;
+  }
+
+  mem = ctf_descriptor->mem;
+  event_mem = mem + TCP_HEADER_SIZE;
+
+  /* Lock mem, datastream and output_stream resources */
+  g_mutex_lock (&ctf_descriptor->mutex);
+  /* Add CTF header */
+  CTF_EVENT_WRITE_HEADER (id, event_mem);
+  /* Write GPU load for each GPU */
+  for (gpu_idx = 0; gpu_idx < gpu_num; ++gpu_idx) {
+    /* Write GPU load */
+    CTF_EVENT_WRITE_FLOAT (gpuload[gpu_idx], event_mem);
+  }
+
+  if (FALSE == ctf_descriptor->file_output_disable) {
+    event_mem = mem + TCP_HEADER_SIZE;
+    fwrite (event_mem, sizeof (gchar), event_size, ctf_descriptor->datastream);
+  }
+
+  if (FALSE == ctf_descriptor->tcp_output_disable) {
+    /* Write the TCP header */
+    TCP_EVENT_HEADER_WRITE (TCP_DATASTREAM_ID, event_size, mem);
+
+    g_output_stream_write (ctf_descriptor->output_stream,
+        ctf_descriptor->mem, event_size + TCP_HEADER_SIZE, NULL, &error);
+  }
+
+  g_mutex_unlock (&ctf_descriptor->mutex);
+}
+
 void
 do_print_proctime_event (event_id id, gchar * elementname, guint64 time)
 {
